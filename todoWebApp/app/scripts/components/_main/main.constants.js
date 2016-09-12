fbdb755bc39817f434703ad57c771abc81f7d3eb
Don Sumayan <don.sumayan@gmail.com>
2016-09-12 09:55:54 +0800

    changed constants SERVICE_URL to point to /tasker-services instead of tasker

--- todoWebApp/app/scripts/components/_main/main.constants.js
+++ todoWebApp/app/scripts/components/_main/main.constants.js
@@ -3,7 +3,7 @@
     angular
         .module("todo")
         .constant('constants', {
-            SERVICE_URL:'http://104.199.192.74/tasker/',
+            SERVICE_URL:'http://104.199.192.74/tasker-services/',
             APP_NAME: 'Tasker'
         });
 })();
